249513d22145e577e43477980646cf9e09c30394
longshine <longxianghe@gmail.com>
2021-09-04 17:14:10 +0800

    Add name to pathes

--- django_authlib_oauth2/urls.py
+++ django_authlib_oauth2/urls.py
@@ -5,9 +5,9 @@ from .views.authorize import AuthorizeView
 from .views.user import LoginView
 
 urlpatterns = [
-    re_path(r'^token$', token.issue),
-    re_path(r'^revoke$', token.revoke),
-    re_path(r'^profile$', user.profile),
+    re_path(r'^token$', token.issue, name='token'),
+    re_path(r'^revoke$', token.revoke, name='revoke'),
+    re_path(r'^profile$', user.profile, name='profile'),
     re_path(r'^login$', LoginView.as_view()),
-    re_path(r'^authorize$', AuthorizeView.as_view()),
+    re_path(r'^authorize$', AuthorizeView.as_view(), name='authorize'),
 ]
